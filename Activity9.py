1e50f4aa556ffd0c9ad7453c055746b5ad7ded45
Elona-Earl <elonaearl17@gmail.com>
2025-09-08 19:15:53 +0800

    Activity9.py

---
+++ Activity9.py
@@ -0,0 +1,11 @@
+a = 5
+b = 3
+
+c = 6
+d = 5
+username='Earl'
+password='Shinnryuu'
+print(username=='Earl')and(password=='Earl')
+print((a>b)and(c<d))
+print((a>b)or(c<d))
+print(not((a>b)or(c<d)))
