ee530dd11dd4f994818dd54c05e2a70b28c13a4e
Elona-Earl <elonaearl17@gmail.com>
2025-09-08 19:16:24 +0800

    Activity10.py

---
+++ Activity10.py
@@ -0,0 +1,11 @@
+username = ("ShinRyuu")
+password = ("Igopko123")
+
+uname = input("Enter your username ---> ")
+pword = input("Enter your password ---> ")
+
+if (uname == username)and(password == pword):
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
+    
