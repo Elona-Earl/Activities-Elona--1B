428e65a9546b7c749bfc875489edf31b56077daf
Elona-Earl <elonaearl17@gmail.com>
2025-09-17 18:08:08 +0800

    codechallenge7.py

---
+++ codechallenge7.py
@@ -0,0 +1,5 @@
+print("Welcome to countdown timer simulator")
+start = eval(input("Enter the starting number for countdown --> "))
+for countdown in range(start,0,-1):
+    print(countdown)
+print("Liftoff!!")
